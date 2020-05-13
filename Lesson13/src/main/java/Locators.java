f6eca80488d89743962e4768611a0f06765e6a02
ingvar <igor.konstantinov.ua@gmail.com>
2020-05-14 00:38:19 +0300

    Lesson13

---
+++ Lesson13/src/main/java/Locators.java
@@ -0,0 +1,18 @@
+public class Locators {
+
+    public static final String HEADER = "#the-selenium-browser-automation-project";
+
+    public static final String SEARCH_TEXT_BOX = "#search-by";
+
+    public static final String CLEAR_SEARCH_BUTTON = "#header-wrapper span > i";
+
+    public static final String INTRODUCTION_LINK = "#sidebar > div.highlightable.ps-container.ps-theme-default.ps-active-y > ul > li:nth-child(2) > a";
+
+    public static final String WEBDRIVER_LINK_IN_THE_TEXT = "//*[@id=\"body-inner\"]/p[5]//a";
+
+    public static final String JAVASCRIPT_TAB = "//*[@id=\"body-inner\"]//label[5]";
+
+    public static final String SELENIUM_HOME_LINK = "//*[@id=\"shortcuts\"]//li[1]/a";
+
+    public static final String NAVIGATION_BUTTON_NEXT = "//*[@id=\"navigation\"]//i";
+}
