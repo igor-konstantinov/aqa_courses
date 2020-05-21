f4fa6277f4ba809556093dea6f0c82ef159b3d97
ingvar <igor.konstantinov.ua@gmail.com>
2020-05-21 22:06:08 +0300

    Lesson 13 fix

--- Lesson13/src/main/java/Locators.java
+++ Lesson13/src/main/java/Locators.java
@@ -6,13 +6,13 @@ public class Locators {
 
     public static final String CLEAR_SEARCH_BUTTON = "#header-wrapper span > i";
 
-    public static final String INTRODUCTION_LINK = "#sidebar > div.highlightable.ps-container.ps-theme-default.ps-active-y > ul > li:nth-child(2) > a";
+    public static final String INTRODUCTION_LINK = "li[data-nav-id=\"/introduction/\"]";
 
-    public static final String WEBDRIVER_LINK_IN_THE_TEXT = "//*[@id=\"body-inner\"]/p[5]//a";
+    public static final String WEBDRIVER_LINK_IN_THE_TEXT = "//*[@id=\"body-inner\"]//a[@href=\"https://selenium.dev/documentation/en/webdriver/\"]";
 
-    public static final String JAVASCRIPT_TAB = "//*[@id=\"body-inner\"]//label[5]";
+    public static final String JAVASCRIPT_TAB = "//label[@for=\"tab4code2\"]";
 
-    public static final String SELENIUM_HOME_LINK = "//*[@id=\"shortcuts\"]//li[1]/a";
+    public static final String SELENIUM_HOME_LINK = "*//a[@href=\"https://selenium.dev/\"]";
 
     public static final String NAVIGATION_BUTTON_NEXT = "//*[@id=\"navigation\"]//i";
 }
